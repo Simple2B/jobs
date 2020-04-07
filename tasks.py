b720b7decfb2d5a27a4bff977b87eaa579f680b2
Flav1us <an215@ukr.net>
2020-04-07 21:12:20 +0300

    + db tasks: renew, print

---
+++ tasks.py
@@ -0,0 +1,28 @@
+from invoke import task  # noqa
+from models import ModelBase, User  # , UserRole
+from session import DbSession, db_session_ctx
+
+
+@task
+def renew_db(_):
+    ModelBase.metadata.drop_all()
+    ModelBase.metadata.create_all()
+    session = DbSession()
+    # role_admin = UserRole('admin')
+    ton = User(name='ton', email='ton@gmail.com', passwd='123', role='admin')
+    # session.add(role_admin)
+    session.add(ton)
+    session.commit()
+
+
+@task
+def print_db(_):
+    with db_session_ctx(read_only=True) as session:
+        all = session.query(User).all()
+        print("database users:")
+        for user in all:
+            print(user)
+        # all = session.query(UserRole).all()
+        # print("user roles:")
+        # for role in all:
+        #     print(role)

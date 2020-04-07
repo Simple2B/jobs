5e3b4534c45cb3f584f6785d04a312c0121e65af
Flav1us <an215@ukr.net>
2020-04-07 17:04:31 +0300

    landing login chassis (old)

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, session
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = "adlfkhLSDHFlkshfsdbfnBSMDNFBSkjweKDFJhsldkjhf"
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+
+    @property
+    def name(self):
+        return self.username.data
+
+    @property
+    def passwd(self):
+        return self.password.data
 
 @app.route("/")
 def home():
-    return("hello, world")
+    if 'role' not in session:
+        session['role'] = 'guest'
+    return render_template("landing.html", form=LoginForm(), role=session['role'])
+
+@app.route("/login", methods=['POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.name=="ton" and form.passwd == "123":
+            return "hello, ton"
+        else:
+            return "no such user"
+
+@app.route("/signup", methods=['POST'])
+def signup():
+    pass

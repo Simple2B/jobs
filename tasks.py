8b382bbad4dcfabff6b3b66d7fb048926ecb8cc4
Flav1us <an215@ukr.net>
2020-04-09 22:20:08 +0300

    new admin

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from invoke import task  # noqa
 from models import ModelBase, User, UserRoleEnum  # , UserRole
 from session import db_session_ctx
+from secret_settings import admin_email, admin_name, admin_password
 
 
 @task
@@ -9,12 +10,12 @@ def renew_db(_):
     ModelBase.metadata.create_all()
     with db_session_ctx(read_only=False) as dsession:
         # role_admin = UserRole('admin')
-        ton = User(name='ton', email='an.malyshko@gmail.com', passwd='123', role=UserRoleEnum.ADMIN)
-        ton.is_email_confirmed = True
+        admin = User(name=admin_name, email=admin_email, passwd=admin_password, role=UserRoleEnum.ADMIN)
+        admin.is_email_confirmed = True
         test_user = User(name='ton_user', email='an.malyshko@gmail.com', passwd='123', role=UserRoleEnum.USER)
         test_user.is_email_confirmed = True
         # session.add(role_admin)
-        dsession.add(ton)
+        dsession.add(admin)
         dsession.add(test_user)
 
 
